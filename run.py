b0e1875382ac717d1720bcdda0076b66924079de
Lucas Pippenger <midpipps@gmail.com>
2016-06-10 01:01:21 -0500

    Adding in the first of the sql injections

--- run.py
+++ run.py
@@ -1,9 +1,12 @@
 from flask import Flask, render_template, request, redirect, url_for
-from setup.db import db, xss
+from setup.db import db, xss, sqlinjection
 import logging
 from logging import StreamHandler
 app = Flask(__name__)
 
+#**************
+#Misc Routes
+#**************
 @app.route('/')
 def index():
     return render_template('index.html')
@@ -12,7 +15,13 @@ def index():
 def reset():
     db.create(True)
     return redirect(url_for('index'))
+#**************
+#End Misc Routes
+#**************
 
+#*************
+#XSS Routes
+#*************
 @app.route('/xss/reflected/', methods=['GET', 'POST'])
 def xss_reflected():
     name=None
@@ -21,7 +30,7 @@ def xss_reflected():
     return render_template('./xss/reflected.html', name=name)
 
 @app.route('/xss/stored/', methods=['GET', 'POST'])
-def stored():
+def xss_stored():
     if request.method == 'POST':
         name = request.form['name']
         comment = request.form['comment']
@@ -30,12 +39,37 @@ def stored():
             xss.addComment(name, comment, parentID)
     all_rows = xss.getComments()
     return render_template('./xss/stored.html', comments=all_rows)
+#**************
+#End XSS Routes
+#**************
 
+#**************
+#SQLI Routes
+#**************
+@app.route('/sqli/simple/', methods=['GET', 'POST'])
+def sqli_simple():
+    comments = None
+    search = None
+    if request.method == 'POST':
+        search = request.form['search']
+    if search:
+        comments = sqlinjection.search(search)
+    return render_template('./sqli/simple.html', comments = comments, search = search)
+#**************
+#End SQLI Routes
+#**************
+
+#**************
+#Filters
+#**************
 @app.template_filter('commentCut')
 def commentCut(comments, id):
     if comments:
         return (x for x in comments if x[4] == id)
     return None
+#**************
+#End Filters
+#**************
 
 if __name__=='__main__':
     logging.basicConfig(level=logging.DEBUG)
--- setup/db/sqlinjection.py
+++ setup/db/sqlinjection.py
@@ -5,7 +5,7 @@ SQLI_DB_NAME = "simpleInjection.db"
 sqli_db_path = None
 #setup the tables and data for the simple sql injection page
 def create(db_path, overwrite=False):
-	global xss_db_path
+	global sqli_db_path
 	sqli_db_path = db_path + SQLI_DB_NAME
 
 	logging.info('Checking SimpleInjection')
@@ -29,7 +29,7 @@ def create(db_path, overwrite=False):
 		conn.execute("INSERT INTO SEARCH (ID, NAME, COMMENT, DATECREATED, SECRET, DISPLAY) \
 					VALUES (3, 'Marvin3', 'Not that anyone cares what I say, but the restaurant is at the *other* end of the Universe.', '2016-11-12 12:00:00', 'OhNoesMySecrets', 1)")
 		conn.execute("INSERT INTO SEARCH (ID, NAME, COMMENT, DATECREATED, SECRET, DISPLAY) \
-					VALUES (4, 'Marvin4', 'I''ve calculated your chance of survival, but I don''t think you''ll like it.', '2015-08-04 12:00:00', 'Okifyoustealme', 1)")
+					VALUES (4, 'Marvin4', 'I''ve calculated your chance of survival, but I don''t think you''ll like it.', '2015-08-04 12:00:00', 'Okifyoustealme', 0)")
 		conn.execute("INSERT INTO SEARCH (ID, NAME, COMMENT, DATECREATED, SECRET, DISPLAY) \
 					VALUES (5, 'Marvin5', 'You think you''ve got problems. What are you supposed to do if you are a manically depressed robot? No, don''t even bother answering. I''m 50,000 times more intelligent than you and even I don''t know the answer.', '2018-09-20 12:00:00', 'NotSoSecret', 1)")
 		conn.execute("INSERT INTO SEARCH (ID, NAME, COMMENT, DATECREATED, SECRET, DISPLAY) \
@@ -37,3 +37,10 @@ def create(db_path, overwrite=False):
 		conn.commit()
 	conn.close()
 	logging.info('SimpleInjection should be good to go')
+
+def search(search):
+	conn = sqlite3.connect(sqli_db_path)
+	cursor = conn.execute("SELECT ID, NAME, COMMENT, DATECREATED FROM SEARCH WHERE DISPLAY <> 0 AND NAME LIKE '%" + search + "%' ORDER BY ID")
+	all_rows = cursor.fetchall()
+	conn.close()
+	return all_rows
